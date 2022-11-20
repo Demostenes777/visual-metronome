a40875c415be8e04c1be9b5ab45c40c28d6db833
Alaitz Mendiola <amendiol@redhat.com>
2022-11-20 18:23:41 +0100

    Add visualization modes

--- metronome.js
+++ metronome.js
@@ -2,20 +2,21 @@ window.last_timestamp = 0;
 window.active_item = 0;
 window.running = false;
 window.timerId = null;
+window.visualizationMode = 1;
 
-function runMetronome() { 
+function runMetronome() {
     let bpms = getBpms();
     let items = getItems();
     let i = getIndex();
     let delay = 0;
-    
+
     if (window.running) {
         clearTimeout(window.timerId);
         delay = Date.now() - window.last_timestamp;
-    } 
+    }
 
     let offset = bpms - delay;
-    
+
     window.timerId = setTimeout(function displayNextItem() {
         displayItem(items[i], i);
         window.last_timestamp = Date.now();
@@ -23,7 +24,7 @@ function runMetronome() {
         window.timerId = setTimeout(displayNextItem, bpms)
     }, offset);
 
-    window.running = true; 
+    window.running = true;
 }
 
 function stopMetronome() {
@@ -33,7 +34,7 @@ function stopMetronome() {
 }
 
 function showItems() {
-    var elements = document.getElementsByClassName( "numbered-item" );
+    var elements = document.getElementsByClassName("numbered-item");
     for (var i = 0; i < elements.length; i++) {
         elements[i].classList.add('d-block');
         elements[i].classList.remove('d-none');
@@ -41,23 +42,43 @@ function showItems() {
 }
 
 function hideItems() {
-    var elements = document.getElementsByClassName( "numbered-item" );
+    var elements = document.getElementsByClassName("numbered-item");
     for (var i = 0; i < elements.length; i++) {
         elements[i].classList.remove('d-block');
+        elements[i].classList.remove('bg-white');
+        elements[i].classList.remove('text-white');
+        elements[i].classList.add('bg-info');
         elements[i].classList.add('d-none');
+        elements[i].classList.add('text-dark');
     }
 }
 
 function displayItem(element, i) {
     hideItems();
-    element.classList.remove('d-none');
-    element.classList.add('d-block');
-    window.active_item = i;
-    let mySound = new Audio('audio/beep-07a.mp3')
-    if (i == 0) {
-        mySound = new Audio('audio/beep-08b.mp3')
-    } 
-    mySound.play();
+    if (hasToBeDisplayed(i)) {
+        element.classList.remove('d-none');
+        element.classList.add('d-block');
+        element.classList.add('bg-info');
+        window.active_item = i;
+        let mySound = new Audio('audio/beep1.mp3')
+        if (i == 0) {
+            mySound = new Audio('audio/beep2.mp3')
+        }
+        mySound.play();
+    } else {
+        element.classList.remove('d-none');
+        element.classList.add('bg-white');
+        element.classList.add('text-white');
+    }
+}
+
+function hasToBeDisplayed(i) {
+    if (window.visualizationMode == 1) {return true;} 
+    if (window.visualizationMode == 2 && i != 0) {return false;}
+    if (window.visualizationMode == 3) {
+        if (i == 1 || i == 3) {return false;}
+    }
+    return true;
 }
 
 function getBpms() {
@@ -90,4 +111,8 @@ function getItems() {
     var fourthItem = document.getElementById("fourth-item");
 
     return [firstItem, secondItem, thirdItem, fourthItem];
+}
+
+function updateVisualization() {
+    window.visualizationMode = document.getElementById("visualization-mode").value;
 }
